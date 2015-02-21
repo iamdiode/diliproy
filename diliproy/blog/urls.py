05d50561f2811d82fa9d110d8b6e695bc407fec2
Dilip Roy <mailme@diliproy.com>
2015-02-21 17:45:29 +0000

    Rebuilding the site

--- diliproy/blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url, patterns, include
-from . import views
-
-urlpatterns = patterns('',
-	url(r'^$', views.BlogIndex.as_view(), name='Index'),
-)
--- diliproy/blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Entry
-
-
-class BlogIndex(generic.ListView):
-	queryset = Entry.objects.published()
-	template = "blog/index.html"
-	paginate_by = 4
