c988bd25085fedeb2d0c08f6a04885ff3d5d7dbe
Dilip Roy <mailme@diliproy.com>
2015-02-22 08:22:15 +0000

    New logic implemented and database recreated

--- diliproy/blog/admin.py
+++ diliproy/blog/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from django_markdown.admin import MarkdownModelAdmin
-from .models import BlogPost, Tag
+from .models import BlogPost, Tag, Category
+
+
+class CategoryAdmin(MarkdownModelAdmin):
+	date_hierarchy = "created"
+	list_display = ("title", "created")
+	search_fields = ("title",)
+	prepopulated_fields = {"slug": ("title",)}
+	readonly_fields = ("created", "updated")
+
+	class Meta:
+		model = Category
 
 
 class TagAdmin(admin.ModelAdmin):
@@ -21,3 +32,4 @@ class BlogPostAdmin(MarkdownModelAdmin):
 
 admin.site.register(BlogPost, BlogPostAdmin)		
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Category, CategoryAdmin)
--- diliproy/blog/models.py
+++ diliproy/blog/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django_markdown.models import MarkdownField
+
+
+class Category(models.Model):
+	title = models.CharField(max_length=120)
+	slug = models.SlugField(unique=True)
+	description = MarkdownField()
+	created = models.DateTimeField(auto_now_add=True, auto_now=False)
+	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse("category", args=[str(self.slug)])
+
 
 
 class Tag(models.Model):
@@ -9,17 +26,33 @@ class Tag(models.Model):
 		return self.tag
 
 
+class PostManager(models.Manager):
+	def all(self):
+		return super(PostManager, self).filter(publish=True)
+
+
 class BlogPost(models.Model):
 	title = models.CharField(max_length=120)
 	body = MarkdownField()
 	slug = models.SlugField(unique=True)
+	category = models.ForeignKey(Category, related_name='categories')
 	tags = models.ManyToManyField(Tag)
 	publish = models.BooleanField(default=True)
+	objects = PostManager()
 	created = models.DateTimeField(auto_now_add=True, auto_now=False)
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse("post", args=[str(self.slug)])
+
+	def get_previous_post(self):
+		return self.get_previous_by_created()
+
+	def get_next_post(self):
+		return self.get_next_by_created()
 
 	class Meta:
 		ordering = ["-created"]
