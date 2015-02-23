3d045ef7c7e907a866df0d0059cee786ec54da30
Dilip Roy <mailme@diliproy.com>
2015-02-23 07:13:44 +0000

    Pagination and categories added

--- diliproy/blog/views.py
+++ diliproy/blog/views.py
@@ -15,12 +15,14 @@ def archive(request):
 	except EmptyPage:
 		# If page is out of range, deliver the last page in the result set
 		posts = paginator.page(paginator.num_pages)
+	categories = Category.objects.all()
 	template = "blog/index.html"
 	return render(request, template, locals())
 
 
 def single_post(request, slug):
 	post = get_object_or_404(BlogPost, slug=slug)
+	categories = Category.objects.all()
 	template = "blog/single.html"
 	return render(request, template, locals())
 
@@ -29,6 +31,16 @@ def category_archive(request, slug):
 	category = get_object_or_404(Category, slug=slug)
 	categories = Category.objects.all()
 	posts = BlogPost.objects.filter(category=category)
+	paginator = Paginator(posts, 6)
+	page = request.GET.get("page")
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer deliver this page
+		posts = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range, deliver the last page in the result set
+		posts = paginator.page(paginator.num_pages)
 	template = "blog/category.html"
 	return render(request, template, locals())
 
