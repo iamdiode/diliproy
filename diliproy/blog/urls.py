6864b52a635827f1016d28c0b8dfeb1c1f68e057
Dilip Roy <mailme@diliproy.com>
2015-02-28 17:07:15 +0000

    With testing modules reinitializing project

--- diliproy/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('blog.views',
-	url(r'^$', 'archive', name='blog_home'),
-	url(r'^post/(?P<slug>[-\w]+)/$', 'single_post', name='blog_post'),
-	url(r'^category/(?P<slug>[-\w]+)/$', 'category_archive', name='category_archive'),
-)
